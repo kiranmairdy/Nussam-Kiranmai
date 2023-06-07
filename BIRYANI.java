e00c72206a5c47e0353bdc23fcf3a2f8eeea17af
Kiranmai Reddy <134801285+kiranmairdy@users.noreply.github.com>
2023-06-07 18:59:24 +0530

    Add files via upload

---
+++ BIRYANI.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		/* package codechef; // don't place package name! */
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	    for(int i=0;i<n;i++){
+	        int a=s.nextInt();
+	        int b=s.nextInt();
+	        System.out.println(a*b);
+	    }
+		// your code goes here
+	}
+}
