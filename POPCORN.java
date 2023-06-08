ce51acf65c8b9e92a8f9c90366c5c1aed635efc7
Kiranmai Reddy <134801285+kiranmairdy@users.noreply.github.com>
2023-06-08 22:04:39 +0530

    Add files via upload

---
+++ POPCORN.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner kb=new Scanner( System.in);
+	   int t=kb.nextInt();
+	   while(t-->0){
+	       int a=3;
+	       int max=-1;
+	       while(a-->0){
+	           int a1=kb.nextInt();
+	           int a2=kb.nextInt();
+	           if(max<a1+a2)
+	             max=a1+a2;
+	       }
+	       System.out.println(max);
+	   }
+	}
+}
